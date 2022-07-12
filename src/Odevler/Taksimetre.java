e5dc0cc042925ad39baf36c7e63f71b21eca7016
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 21:43:46 +0300

    güncelleme

--- src/Odevler/Taksimetre.java
+++ src/Odevler/Taksimetre.java
@@ -6,6 +6,26 @@ public class Taksimetre {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double a,b,c;
+
+        System.out.println("Hos Geldiniz, KM Basince Ucret: 2.20 TL'dir.");
+        System.out.println("Taksi Acilis Fiyati: 10 TL'dir.");
+        System.out.println("Ayrica min. Odeme: 20 TL'dir.");
+        System.out.print("Taksi ile gidilen mesafeyi yaziniz: ");
+        a= input.nextDouble();
+
+        b= 10;
+        c=b+(a*2.2);
+
+        c= c>=20 ? c : 20;
+        System.out.println("Ucret: " + c + "TL");
+
+
+
+
+
+
+
 
 
     }
