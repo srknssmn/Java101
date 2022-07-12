f7ba18de4f0649f3ed1a95cd709cb27241a21fc5
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 16:29:09 +0300

    güncelleme

---
+++ src/Odevler/KdvHesaplama.java
@@ -0,0 +1,14 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class KdvHesaplama {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+    }
+}
