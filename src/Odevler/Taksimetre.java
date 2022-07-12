992566792b0b6222a27af889a8fb58e9b57ab155
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 21:21:40 +0300

    güncelleme

---
+++ src/Odevler/Taksimetre.java
@@ -0,0 +1,12 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+    }
+}
