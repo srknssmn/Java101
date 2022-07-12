f9c3d5fbe38e88e6bc2c92af7156f048e1a6d1f2
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 15:57:15 +0300

    güncelleme

--- src/Odevler/NotOrtalamasi.java
+++ src/Odevler/NotOrtalamasi.java
@@ -6,6 +6,39 @@ public class NotOrtalamasi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // Değişkenleri oluştur.
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        // Kullanıcıdan değerleri al.
+
+        System.out.print("Matematik notunuz: ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunuz: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya notunuz: ");
+        kimya = input.nextInt();
+
+        System.out.print("Turkce notunuz: ");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih notunuz: ");
+        tarih = input.nextInt();
+
+        System.out.print("Muzik notunuz: ");
+        muzik = input.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
+        double sonuc = toplam / 6.0;
+
+        System.out.println("Not Ortalamaniz: " + sonuc);
+
+
+
+
+
+
 
 
 
