bb25cfb1794343463b946820665f80ce4e7a2fed
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 16:12:56 +0300

    guncelleme

--- src/Odevler/NotOrtalamasi.java
+++ src/Odevler/NotOrtalamasi.java
@@ -35,7 +35,7 @@ public class NotOrtalamasi {
         System.out.println("Not Ortalamaniz: " + sonuc);
 
         int gecernot = 60;
-        boolean kosul = sonuc > gecernot;
+        boolean kosul = sonuc >= gecernot;
 
         String genelSonuc = kosul ? "Sinifi Gecti" : "Sinifta Kaldi";
         System.out.println(genelSonuc);
