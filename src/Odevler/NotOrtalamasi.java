1d3a1bea2110cbe61ab99a60200060567ef7a267
Serkan Sisman <srknssmn@gmail.com>
2022-07-12 15:28:03 +0300

    güncelleme

---
+++ src/Odevler/NotOrtalamasi.java
@@ -0,0 +1,15 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+    }
+
+
+}
