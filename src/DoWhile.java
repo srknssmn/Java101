5ee98a0da125bf651070263aeec38cecaa352e89
Serkan Sisman <srknssmn@gmail.com>
2022-07-16 02:21:16 +0300

    güncelleme

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -3,6 +3,7 @@ public class DoWhile {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
 
+        /*
         int pass;
         boolean kosul=true;
 
@@ -13,8 +14,20 @@ public class DoWhile {
                 System.out.print("Sifre dogru");
                 kosul=false;
             }else{
-                System.out.print("Yanlis Tekrar ");
+                System.out.println("Yanlis");;
             }
         }while(kosul);
+        */
+
+        int i=1, j=1;
+        while(i<3)
+        {
+            do
+            {
+                System.out.print(j + ",");
+                j++;
+            }while(j<4);
+            i++;
+        }
     }
 }
