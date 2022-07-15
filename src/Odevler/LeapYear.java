1fde1b098cbe4060073278b28018c8bda12dfceb
Serkan Sisman <srknssmn@gmail.com>
2022-07-15 19:51:06 +0300

    güncelleme

--- src/Odevler/LeapYear.java
+++ src/Odevler/LeapYear.java
@@ -4,12 +4,12 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int year,leapYear;
+        int year;
 
         System.out.print("Bir yil giriniz: ");
         year= input.nextInt();
 
-        if ((year%4==00)&&!((year%100==0)&&!(year%400==00))){
+        if ((year%4==0)&&!((year%100==0)&&!(year%400==0))){
             System.out.println(+year+ "\t Bir artik yildir!");
         }else{
             System.out.println(+year+ "\t Bir artik yil degildir!");
